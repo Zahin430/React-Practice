7660b49f58d53dfffea0b9aaffa60f9251916ee8
Zahin430 <zhprangon@mun.ca>
2019-04-28 16:03:03 -0230

    Added some comments for lifecycle methods

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ import ReactDOM from 'react-dom';
 
 
 class App extends React.Component {
+    // Good place to do one-time setup
+    // No data loading inside the constructor but it is a bad practice
+    // For clarity purposes
     constructor(props) {
         //Used to initialize state
         super(props);
+
 
         // This is called at first to do direct assignment or initialize
         // to this state
@@ -24,9 +28,20 @@ class App extends React.Component {
             }
         );
     }
-    
+
+    // Method to do data-loading or initial data loading
+    componentDidMount() {
+        console.log('My component was rendered to the screen');
+    }
+
+    // Waits for update and then render
+    // Method to do more data loading when state/props change
+    componentDidUpdate() {
+        console.log('My component was just updated - it rerendered');
+    }
+
     // The state can be used inside of the render function
-    //Render method should not contain any
+    // Render method should not contain anythin but jsx
     render() {  
         return (
         <div>Latitude: {this.state.lat}
